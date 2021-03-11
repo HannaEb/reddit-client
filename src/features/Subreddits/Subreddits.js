994e9a3c4fce171c88b1f22f196aa8196d568bc0
Hanna <hannaeberhoefer@yahoo.com>
2021-03-11 22:31:32 +0000

    Add icons to subreddits

--- src/features/Subreddits/Subreddits.js
+++ src/features/Subreddits/Subreddits.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import './Subreddits.css'
+
 
 export const Subreddits = () => {
     const subreddits = useSelector(state => state.subreddits)
-    const renderedSubreddits = subreddits.map(subreddit => (
-        <div key={subreddit.id}>
-            <h3>{subreddit.topic}</h3>
-        </div>
-    ))
+
     return (
-        <aside className="subreddits">
-            <h2>Subreddits</h2>
-            {renderedSubreddits}
-        </aside>
+        <>
+            <div id="subreddits-container">
+                <h2>Subreddits</h2>
+                <ul>
+                    {subreddits.map((subreddit) => (
+                        <li key={subreddit.id}>
+                            <button>
+                                <div className="subreddit-icon">{subreddit.icon}</div>
+                                <div>{subreddit.topic}</div>
+                            </button>
+                        </li>
+                    ))}     
+                </ul>
+            </div>
+        </>
     )
 }
--- src/slices/subRedditSlice.js
+++ src/slices/subRedditSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { SiReddit } from 'react-icons/si'
 
 const initialState = [
-    { id: '1', topic: 'News' },
-    { id: '2', topic: 'Technology' },
-    { id: '3', topic: 'Wildlife'},
-    { id: '4', topic: 'London'}
+    { id: '1', topic: 'News', icon: <SiReddit size={28} /> },
+    { id: '2', topic: 'Technology', icon: <SiReddit size={28} /> },
+    { id: '3', topic: 'Wildlife', icon: <SiReddit size={28} />},
+    { id: '4', topic: 'London', icon: <SiReddit size={28} />}
 ]
 
 const subRedditSlice = createSlice({
