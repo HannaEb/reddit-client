07ad850d688f1b7bfb729bd22011f373dbe091f7
Hanna <hannaeberhoefer@yahoo.com>
2021-03-11 19:56:53 +0000

    Organise App.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,24 @@ import { Subreddits } from './features/Subreddits/Subreddits'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <>
-                <header>
-                  <Header />
-                </header>
-                <nav>
-                  <Subreddits />
-                </nav>
-              </>
-            )}
-          />
-          <Redirect to="/" />
-        </Switch>
-      </div>
-    </Router>
-  );
+      <Router>
+        <div className="App">
+          <Header />
+          <section>
+            <aside>
+              <Subreddits />
+            </aside>
+            <main>
+              <Switch>
+                <Route exact Path="/">
+                  {/* <Feed /> */}
+                </Route>
+              </Switch>
+            </main>
+          </section>
+        </div>
+      </Router>
+  )
 }
 
 export default App
