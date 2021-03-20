82de32a17692acb6efd88ccea7ab1fc8fe429810
Hanna <hannaeberhoefer@yahoo.com>
2021-03-20 15:11:19 +0000

    Basic comment styles

--- src/features/Comment/Comment.js
+++ src/features/Comment/Comment.js
@@ -5,12 +5,12 @@ const Comment = (props) => {
     const { comment } = props
     return (
         <div className="comment">
-            <div className="comment-text">
-                <p>Content</p>
-            </div>
             <div className="comment-refs">
                 <p className="comment-author">Author</p>
                 <p className="comment-created">Date</p>
+            </div>
+            <div className="comment-text">
+                <p>Content</p>
             </div>
         </div>
     )
