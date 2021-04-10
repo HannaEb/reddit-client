064f76a81496bb46062b7c9e22f70c9eaf447264
Hanna <hannaeberhoefer@yahoo.com>
2021-04-10 13:25:59 +0100

    Test app

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { shallow } from 'enzyme';
 import store from './app/store';
 import App from './App';
 
-// test('renders learn react link', () => {
+it('renders without crashing', () => {
+    shallow(<App />);
+});
+
+// it('renders learn react link', () => {
 //   const { getByText } = render(
 //     <Provider store={store}>
 //       <App />
 //     </Provider>
 //   );
 
-//   expect(getByText(/learn/i)).toBeInTheDocument();
+//   expect(getByText('Subreddits')).toBeInTheDocument();
 // });
